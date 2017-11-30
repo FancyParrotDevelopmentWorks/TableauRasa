edf2d8d4e18727a20d6c6ba2c3b917f58ab7f7c1
M Woods <mkowoods@gmail.com>
2017-11-29 23:39:56 -0800

    commit for scripting adding users to group

--- rasa/api.py
+++ rasa/api.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import time
 API_VERSION = 2.5
 BASE_HEADER = {
     'accept': 'application/json',
@@ -48,7 +48,17 @@ class TableauJSONAPI(object):
         resp.raise_for_status()
         return resp.json()
 
-
+    def _handle_post(self, route, payload, name, wait):
+        try:
+            resp = self._post_json(route, payload=payload)
+            print 'pausing', wait, 'seconds to let index update'
+            time.sleep(wait) #done so that we give the index time to update
+            return resp
+        except requests.HTTPError as e:
+            if str(e).startswith('409'):
+                print '{url} post: {name} Already Exists'.format(url=route, name=name)
+            else:
+                raise e
 
     def _clean_site_name(self, site_name):
         """
@@ -101,6 +111,11 @@ class TableauJSONAPI(object):
         self.site_url = None
         return None
 
+    def auth_change_site(self, site_url=""):
+
+        self.auth_signout()
+        self.auth_signin(site_url)
+
 
     def sites_query_site(self, site_url = None):
         """
@@ -125,25 +140,43 @@ class TableauJSONAPI(object):
             site_id = self.site_id, get_usage_information = get_usage_information)
         return self._get_json(url_route)
 
-
-    def users_get_users_on_site(self):
-        url_route = '/sites/{site_id}/users'.format(site_id = self.site_id)
+    def users_get_users_on_site(self, page_size = 1000, page_number = 1):
+        url_route = '/sites/{site_id}/users?pageSize={page_size}&pageNumber={page_number}' \
+            .format(site_id = self.site_id, page_size = page_size, page_number = page_number)
         return self._get_json(url_route)
 
-
     def users_query_user_on_site(self, user_id):
-        url_route  = '/sites/{site_id}/users/{user_id}'.format(site_id = self.site_id, user_id = user_id)
-        return   self._get_json(url_route)
+        url_route  = '/sites/{site_id}/users/{user_id}'.format(user_id = user_id)
+        return self._get_json(url_route)
 
-
-    def groups_create_group(self, group_name):
-        url_route = '/api/api-version/sites/{site_id}/groups'.format(site_id = self.site_id)
+    def users_add_user_to_site(self, user_name, site_role = 'Interactor', sleep_in_seconds = 2):
+        url_route = '/sites/{site_id}/users'.format(site_id=self.site_id)
         payload = {
-            'new-tableau-server-group-name': group_name
+            'user': {
+                'name': user_name,
+                'siteRole': site_role
+            }
         }
+        return self._handle_post(route=url_route, payload=payload, name=user_name, wait=sleep_in_seconds)
 
-        return self._post_json(url_route, payload=payload)
+    def groups_query_groups(self):
+        url_route = '/sites/{site_id}/groups'.format(site_id = self.site_id)
+        return self._get_json(url_route)
 
+    def groups_create_group(self, group_name, sleep_in_seconds = 2):
+        url_route = '/sites/{site_id}/groups'.format(site_id = self.site_id)
+        payload = {
+            'group': {'name': group_name}
+        }
+        return self._handle_post(route=url_route, payload=payload, name=group_name, wait=sleep_in_seconds)
+
+    def groups_add_user_to_group(self, user_id, group_id, sleep_in_seconds = 2):
+        url_route = '/sites/{site_id}/groups/{group_id}/users'.format(site_id=self.site_id, group_id=group_id)
+        print url_route
+        payload = {
+            'user': {'id': user_id}
+        }
+        return self._handle_post(route=url_route, payload=payload, name=user_id, wait=sleep_in_seconds)
 
     def get_url(self, url):
         """
@@ -156,12 +189,31 @@ class TableauJSONAPI(object):
         resp.raise_for_status()
         return resp
 
+    def get_group_id_from_groupname(self, group_name_list=[]):
+        groups = self.groups_query_groups()['groups']['group']
+        name_id_hash = {group['name'].lower(): group['id'] for group in groups}
+        return [[group.lower(), name_id_hash.get(group.lower())] for group in group_name_list]
 
+    def get_user_id_from_name(self, user_name_list=[]):
+        site_users = self.users_get_users_on_site()['users']['user']
+        user_id_hash = {user['name'].lower(): user['id'] for user in site_users}
 
+        # preprocess un_list => lower username and pop from email if email is used
+        pp_ = lambda un: un.lower().split('@')[0]
+        return [[un, user_id_hash.get(un)] for un in map(pp_, user_name_list)]
 
-if __name__ == "__main__":
+    def script_batch_add_users_to_group(self, user_names=[], group_name=None, force = False):
 
-    sess = TableauJSONAPI(config_file='private_creds.json')
-    sess.auth_signin(site_url="")
-    print(json.dumps(sess.sites_query_views_for_site(include_usage_stats=True), indent=4))
-    sess.auth_signout()
+        group_id = self.get_group_id_from_groupname(group_name_list=[group_name])[0][1]
+        if not group_id:
+            if force:
+                self.groups_create_group(group_name)
+            raise ValueError('Group %s Does Not Exist'%(str(group_name)))
+        for un, uid in self.get_user_id_from_name(user_name_list=user_names):
+            try:
+                res = self.groups_add_user_to_group( uid, group_id)
+                if res:
+                    print 'Created Username', un, 'on Group', group_name
+            except Exception as e:
+                print e
+
